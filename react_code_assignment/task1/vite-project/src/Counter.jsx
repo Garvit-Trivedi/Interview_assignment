e09f8d3b5fe878d19e3728071e9302f4cf7cb919
Garvit-Trivedi <garvit.trivedi.cg@gmail.com>
2025-04-11 00:00:56 +0530

    completed task-1 of the react

---
+++ react_code_assignment/task1/vite-project/src/Counter.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './Counter.css'; 
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(prev => prev + 1);
+  const decrement = () => setCount(prev => prev - 1);
+  const reset = () => setCount(0);
+
+  return (
+    <div className="counter-container">
+      <h2>Counter: {count}</h2>
+      <div className="btn-group">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
